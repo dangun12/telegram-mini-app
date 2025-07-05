6b8f44499244aaa456ccfb917a5e1b13a9eac08d
dangun12 <danylo.dovgun@gmail.com>
2025-07-05 15:32:13 +0300

    Update save.js

--- pages/api/save.js
+++ pages/api/save.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
       body: JSON.stringify(req.body),
     });
 
+    
     const text = await response.text();
     res.status(200).send(text);
   } catch (error) {
