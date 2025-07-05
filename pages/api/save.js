bccb96d4a2e29cc26c658546544a767c07d53f87
dangun12 <danylo.dovgun@gmail.com>
2025-07-05 15:57:57 +0300

    Update save.js

--- pages/api/save.js
+++ pages/api/save.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/ТВОЙ_ID/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbweWzP0BEnFWdHUWqeO_hVLgbWjB5xTwKWioyei-ysDVnlu8WX4aWyXJYhFR_rcZwpG/exec', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
